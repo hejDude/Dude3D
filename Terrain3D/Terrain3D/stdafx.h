76b1fa2004b30945b0bcf19679532503b48b7be4
hejDude <amba07dude@gmail.com>
2014-03-24 20:21:23 +0100

    Dodano projekt utworzony w VS2010

---
+++ Terrain3D/Terrain3D/stdafx.h
@@ -0,0 +1,42 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#ifndef _stdafx_h_
+#define _stdafx_h_
+
+
+//#include <iostream>
+//#include <stdio.h>
+//#include <stdlib.h>
+
+//#include <string>
+//#include <tchar.h>
+//#include <time.h>
+//#include <windows.h>
+
+//using namespace std;
+
+
+
+// TODO: reference additional headers your program requires here
+
+/************************************************************************************************************************************/
+
+/************************************************************************************************************************************/
+
+
+#endif
+
+
+/*-------------------------------------------------------------------------------------------
+*#include <cassert> :
+	Dostarcza makro, które mo¿e byæ u¿ywane jako standardowe narzêdzie debuguj¹ce : "void assert (int expression);"
+	Gdy argument expression jest równy 0, na standardowy strumieñ b³êdów zostaje wypisana informacja oraz zostaje 
+	wywo³ana funkcja abort, która przerywa dzia³anie programu. - http://4programmers.net/C/Biblioteka_standardowa/Cassert
+
+*#include <map> :
+	Standardowy kontener, który wykorzystujemy do gromadzenia danych o naszych sprite'ach :
+	 http://pl.wikibooks.org/wiki/C++/Map
+-------------------------------------------------------------------------------------------*/
